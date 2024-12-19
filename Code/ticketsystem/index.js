dbb3032e6c1898f8e94f000a76c958dd9f66c84f
justin <justin.rasi@edu.tbz.ch>
2024-12-19 14:22:13 +0100

    revert

--- Code/ticketsystem/index.js
+++ Code/ticketsystem/index.js
@@ -1,29 +1,8 @@
 const express = require('express');
-
 const app = express();
-
-app.use(express.json());
-
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
-});
-
-let tickets = [];
-let currentId = 1;
-
-function validateTicket(ticket) {
-  if (!ticket.title || !ticket.description || !ticket.status) {
-    return false;
-  }
-  return true;
-}
-
-app.post('/tickets', (req, res) => {
-  const ticket = req.body;
-  if (!validateTicket(ticket)) {
-    return res.status(400).json({ error: 'Invalid ticket data' });
-  }
-  ticket.id = currentId++;
-  tickets.push(ticket);
-  res.json(ticket);
 });
